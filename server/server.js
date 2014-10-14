5ff0424faf7b498340ddd4b5ae14de4592ad28d3
manuel <bboykbra@gmail.com>
2014-10-14 22:44:45 +0200

    added ping to maintain wifi alive in raspberry pi

--- server/server.js
+++ server/server.js
@@ -1,33 +1,40 @@
 /*global require: false, console: false*/
-var express = require('express'),
+var express = require("express"),
     app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    path = require('path'),
+    server = require("http").createServer(app),
+    io = require("socket.io").listen(server),
+    path = require("path"),
     Controller,
-    events = require('events'),
+    events = require("events"),
+    Ping = require("ping-wrapper")
     serverConfig = require("./config.json");
 
 server.listen(serverConfig.port);
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
     "use strict";
     res.sendfile(path.resolve(__dirname + "/../client") + "/index.html");
 });
 
-app.use(express.static(path.resolve(__dirname + "/../client") + '/'));
+app.use(express.static(path.resolve(__dirname + "/../client") + "/"));
 
-Controller = require('./app/Controller').Controller;
+Controller = require("./app/Controller").Controller;
 
 //TODO do that in the controller??
 Controller.getManagers().pantalla.on("temperatura", function (value) {
     io.sockets.send(JSON.stringify({"temperatura": value}));
 });
 
-io.sockets.on('connection', function (socket) {
+//hearbeat for raspberry pi wifi
+Ping.configure();
+setInterval(function () {
+    var ping = new Ping("192.168.1.1");
+}, 120000);
+
+io.sockets.on("connection", function (socket) {
     "use strict";
 
-    socket.on('message', function (message) {
+    socket.on("message", function (message) {
         var data, type, mgrFunction, manager;
 
         data = JSON.parse(message);
