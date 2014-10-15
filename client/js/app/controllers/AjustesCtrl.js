596d99dca22d06a58428645496f1bf9dde0b1fa9
manuel <bboykbra@gmail.com>
2014-10-15 22:00:36 +0200

    delete $scope.$apply();

--- client/js/app/controllers/AjustesCtrl.js
+++ client/js/app/controllers/AjustesCtrl.js
@@ -22,12 +22,10 @@ angular.module("app")
         $scope.save = function () {
             connector.sendMessage("pantalla/setConfig", $scope.settings);
             $scope.cssClasses.isSaved = "saved";
-            $scope.$apply();
         };
 
         $scope.onChange = function () {
             $scope.cssClasses.isSaved = "unsaved";
-            $scope.$apply();
         };
 
         if (connector.isReady()) {
